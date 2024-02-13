992f2869afb3e784642f373386e9e48b36702ca1
QuantaJi <quanta.guangda.ji@gmail.com>
2024-02-13 13:46:34 +0100

    preprocess code for scannet

---
+++ labelmaker_scripts/preprocess_scannet.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+#SBATCH --job-name="labelmaker-pointcept-scannet-preprocess"
+#SBATCH --output=labelmaker_pointcept_scannet_preprocess.out
+#SBATCH --time=12:00:00
+#SBATCH --ntasks=1
+#SBATCH -A ls_polle
+#SBATCH --cpus-per-task=32
+#SBATCH --mem-per-cpu=2G
+#SBATCH --tmp=32G
+
+set -e
+
+module purge
+module load eth_proxy
+
+export PATH="/cluster/project/cvg/labelmaker/miniconda3/bin:${PATH}"
+
+env_name=labelmaker-pointcept
+eval "$(conda shell.bash hook)"
+conda activate $env_name
+
+conda_home="$(conda info | grep "active env location : " | cut -d ":" -f2-)"
+conda_home="${conda_home#"${conda_home%%[![:space:]]*}"}"
+
+RAW_SCANNET_DIR=/cluster/project/cvg/Shared_datasets/scannet
+PROCESSED_SCANNET_DIR=/cluster/project/cvg/labelmaker/ptv3_preprocessed_datasets/scannet
+
+# RAW_SCANNET_DIR: the directory of downloaded ScanNet v2 raw dataset.
+# PROCESSED_SCANNET_DIR: the directory of the processed ScanNet dataset (output dir).
+python pointcept/datasets/preprocessing/scannet/preprocess_scannet.py --dataset_root ${RAW_SCANNET_DIR} --output_root ${PROCESSED_SCANNET_DIR}
