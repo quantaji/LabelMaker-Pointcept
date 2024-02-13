cb1ee6edb4c091a0af2c2955be14a10ffd47c4b4
QuantaJi <quanta.guangda.ji@gmail.com>
2024-02-13 10:42:13 +0100

    add environment file

---
+++ labelmaker_scripts/activate_env.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+module purge
+module load gcc/11.4.0 cuda/12.1.1 eth_proxy
+export PATH="/cluster/project/cvg/labelmaker/miniconda3/bin:${PATH}"
+env_name=labelmaker-pointcept
+eval "$(conda shell.bash hook)"
+conda activate $env_name
+conda_home="$(conda info | grep "active env location : " | cut -d ":" -f2-)"
+conda_home="${conda_home#"${conda_home%%[![:space:]]*}"}"
+
+which python
+
+export CUDA_HOST_COMPILER="$conda_home/bin/gcc"
+export CUDA_PATH="$conda_home"
+export CUDA_HOME=$CUDA_PATH
---
+++ labelmaker_scripts/install_env_local.sh
@@ -0,0 +1,45 @@
+set -e
+
+env_name=labelmaker-pointcept
+conda create --name $env_name --yes python=3.9
+eval "$(conda shell.bash hook)"
+conda activate $env_name
+
+INSTALLED_GCC_VERSION="9.5.0"
+INSTALLED_CUDA_VERSION="11.3.1"
+INSTALLED_CUDA_ABBREV="cu113"
+INSTALLED_PYTORCH_VERSION="1.12.1"
+INSTALLED_TORCHVISION_VERSION="0.13.1"
+INSTALLED_TORCHAUDIO_VERSION="0.12.1"
+
+conda install -y -c conda-forge gxx=${INSTALLED_GCC_VERSION} sysroot_linux-64=2.17 libcxx
+conda install -y -c "nvidia/label/cuda-${INSTALLED_CUDA_VERSION}" cuda
+
+pip install torch==${INSTALLED_PYTORCH_VERSION} torchvision==${INSTALLED_TORCHVISION_VERSION} torchaudio==${INSTALLED_TORCHAUDIO_VERSION} --index-url https://download.pytorch.org/whl/${INSTALLED_CUDA_ABBREV}
+
+pip install sharedarray tensorboard tensorboardx yapf addict einops scipy plyfile termcolor timm torch-geometric spconv-cu113 h5py pyyaml numpy
+
+pip install torch-scatter==2.1.0 torch-sparse==0.6.16 torch-cluster==1.6.0 --index-url "" -f "https://data.pyg.org/whl/torch-${INSTALLED_PYTORCH_VERSION}%2B${INSTALLED_CUDA_ABBREV}.html"
+
+cd libs/pointops
+
+conda_home="$(conda info | grep "active env location : " | cut -d ":" -f2-)"
+conda_home="${conda_home#"${conda_home%%[![:space:]]*}"}"
+
+conda deactivate
+conda activate ${env_name}
+
+which nvcc
+nvcc --version
+
+export BUILD_WITH_CUDA=1
+export CUDA_HOST_COMPILER="$conda_home/bin/gcc"
+export CUDA_PATH="$conda_home"
+export PATH="$conda_home/bin:$PATH"
+export CUDA_HOME=$CUDA_PATH
+export FORCE_CUDA=1
+export MAX_JOBS=6
+export TORCH_CUDA_ARCH_LIST="6.0 6.1 6.2 7.0 7.2 7.5 8.0 8.6"
+
+python setup.py install
+cd ../..
