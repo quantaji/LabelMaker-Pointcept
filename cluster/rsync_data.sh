f2613c58ca86121e071c8a2dfd8d0c4e9701aa38
QuantaJi <quanta.guangda.ji@gmail.com>
2024-05-22 14:12:47 +0200

    add new joint training config

--- cluster/rsync_data.sh
+++ cluster/rsync_data.sh
@@ -2,4 +2,4 @@
 rsync -r --ignore-existing -v -e ssh guanji@euler.ethz.ch:/cluster/project/cvg/labelmaker/LabelMaker-Pointcept/data/* /home/guangda/repos/LabelMaker-Pointcept/data/
 
 # then rsync all data from herman's zurich machine to cluster
-rsync -r --ignore-existing -v -e ssh  guangda@129.132.245.59:/home/guangda/repos/LabelMaker-Pointcept/data/* /home/blum/LabelMaker-Pointcept/data/
+rsync -r --ignore-existing -v -e ssh  guangda@129.132.245.59:/home/guangda/repos/LabelMaker-Pointcept/data/* /raid/LabelMaker-Pointcept/data/
