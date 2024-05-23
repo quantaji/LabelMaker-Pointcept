39136bbfae74f552103a08908b4c89cf3556729a
QuantaJi <quanta.guangda.ji@gmail.com>
2024-05-23 06:50:18 +0200

    add code for scannetpp joint training

--- configs/scannetpp/semseg-pt-v3m1-2-ppt-extreme-alc.py
+++ configs/scannetpp/semseg-pt-v3m1-2-ppt-extreme-alc.py
@@ -85,64 +85,6 @@ data = dict(
     train=dict(
         type="ConcatDataset",
         datasets=[
-            # Structured3D
-            dict(
-                type="Structured3DDataset",
-                split=["train", "val", "test"],
-                data_root="data/structured3d",
-                transform=[
-                    dict(type="CenterShift", apply_z=True),
-                    dict(
-                        type="RandomDropout",
-                        dropout_ratio=0.2,
-                        dropout_application_ratio=0.2,
-                    ),
-                    # dict(type="RandomRotateTargetAngle", angle=(1/2, 1, 3/2), center=[0, 0, 0], axis="z", p=0.75),
-                    dict(
-                        type="RandomRotate",
-                        angle=[-1, 1],
-                        axis="z",
-                        center=[0, 0, 0],
-                        p=0.5,
-                    ),
-                    dict(type="RandomRotate", angle=[-1 / 64, 1 / 64], axis="x", p=0.5),
-                    dict(type="RandomRotate", angle=[-1 / 64, 1 / 64], axis="y", p=0.5),
-                    dict(type="RandomScale", scale=[0.9, 1.1]),
-                    # dict(type="RandomShift", shift=[0.2, 0.2, 0.2]),
-                    dict(type="RandomFlip", p=0.5),
-                    dict(type="RandomJitter", sigma=0.005, clip=0.02),
-                    dict(
-                        type="ElasticDistortion",
-                        distortion_params=[[0.2, 0.4], [0.8, 1.6]],
-                    ),
-                    dict(type="ChromaticAutoContrast", p=0.2, blend_factor=None),
-                    dict(type="ChromaticTranslation", p=0.95, ratio=0.05),
-                    dict(type="ChromaticJitter", p=0.95, std=0.05),
-                    # dict(type="HueSaturationTranslation", hue_max=0.2, saturation_max=0.2),
-                    # dict(type="RandomColorDrop", p=0.2, color_augment=0.0),
-                    dict(
-                        type="GridSample",
-                        grid_size=0.02,
-                        hash_type="fnv",
-                        mode="train",
-                        return_grid_coord=True,
-                    ),
-                    dict(type="SphereCrop", sample_rate=0.8, mode="random"),
-                    dict(type="SphereCrop", point_max=204800, mode="random"),
-                    dict(type="CenterShift", apply_z=False),
-                    dict(type="NormalizeColor"),
-                    # dict(type="ShufflePoint"),
-                    dict(type="Add", keys_dict={"condition": "Structured3D"}),
-                    dict(type="ToTensor"),
-                    dict(
-                        type="Collect",
-                        keys=("coord", "grid_coord", "segment", "condition"),
-                        feat_keys=("color", "normal"),
-                    ),
-                ],
-                test_mode=False,
-                loop=1,  # sampling weight
-            ),
             # ScanNet
             dict(
                 type="ScanNet200Dataset",
@@ -200,64 +142,6 @@ data = dict(
                 test_mode=False,
                 loop=1,  # sampling weight
             ),
-            # S3DIS
-            # dict(
-            #     type="S3DISDataset",
-            #     split=("Area_1", "Area_2", "Area_3", "Area_4", "Area_6"),
-            #     data_root="data/s3dis",
-            #     transform=[
-            #         dict(type="CenterShift", apply_z=True),
-            #         dict(
-            #             type="RandomDropout",
-            #             dropout_ratio=0.2,
-            #             dropout_application_ratio=0.2,
-            #         ),
-            #         # dict(type="RandomRotateTargetAngle", angle=(1/2, 1, 3/2), center=[0, 0, 0], axis="z", p=0.75),
-            #         dict(
-            #             type="RandomRotate",
-            #             angle=[-1, 1],
-            #             axis="z",
-            #             center=[0, 0, 0],
-            #             p=0.5,
-            #         ),
-            #         dict(type="RandomRotate", angle=[-1 / 64, 1 / 64], axis="x", p=0.5),
-            #         dict(type="RandomRotate", angle=[-1 / 64, 1 / 64], axis="y", p=0.5),
-            #         dict(type="RandomScale", scale=[0.9, 1.1]),
-            #         # dict(type="RandomShift", shift=[0.2, 0.2, 0.2]),
-            #         dict(type="RandomFlip", p=0.5),
-            #         dict(type="RandomJitter", sigma=0.005, clip=0.02),
-            #         dict(
-            #             type="ElasticDistortion",
-            #             distortion_params=[[0.2, 0.4], [0.8, 1.6]],
-            #         ),
-            #         dict(type="ChromaticAutoContrast", p=0.2, blend_factor=None),
-            #         dict(type="ChromaticTranslation", p=0.95, ratio=0.05),
-            #         dict(type="ChromaticJitter", p=0.95, std=0.05),
-            #         # dict(type="HueSaturationTranslation", hue_max=0.2, saturation_max=0.2),
-            #         # dict(type="RandomColorDrop", p=0.2, color_augment=0.0),
-            #         dict(
-            #             type="GridSample",
-            #             grid_size=0.02,
-            #             hash_type="fnv",
-            #             mode="train",
-            #             return_grid_coord=True,
-            #         ),
-            #         dict(type="SphereCrop", sample_rate=0.6, mode="random"),
-            #         dict(type="SphereCrop", point_max=204800, mode="random"),
-            #         dict(type="CenterShift", apply_z=False),
-            #         dict(type="NormalizeColor"),
-            #         dict(type="ShufflePoint"),
-            #         dict(type="Add", keys_dict={"condition": "S3DIS"}),
-            #         dict(type="ToTensor"),
-            #         dict(
-            #             type="Collect",
-            #             keys=("coord", "grid_coord", "segment", "condition"),
-            #             feat_keys=("color", "normal"),
-            #         ),
-            #     ],
-            #     test_mode=False,
-            #     loop=1,  # sampling weight
-            # ),
             # ScanNetPPDataset
             dict(
                 type="ScanNetPPDataset",
@@ -356,6 +240,7 @@ data = dict(
                     ),
                 ],
                 test_mode=False,
+                loop=2,
             ),
         ],
     ),
