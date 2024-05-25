02974700e218a324612a6e0217023f81960be61a
QuantaJi <quanta.guangda.ji@gmail.com>
2024-05-25 06:00:31 +0200

    code for testing

--- cluster/test.sh
+++ cluster/test.sh
@@ -16,11 +16,14 @@ cd /raid/LabelMaker-Pointcept
 
 INTERPRETER_PATH=/home/blum/miniconda3/envs/labelmaker-pointcept/bin/python
 
-export CUDA_VISIBLE_DEVICES="0"
+# export CUDA_VISIBLE_DEVICES="0"
+export CUDA_VISIBLE_DEVICES="0,1,2,3"
 
-NUM_GPU=1
+# NUM_GPU=1
+NUM_GPU=4
 DATASET_NAME=scannetpp
-EXP_NAME=alc_pretrain
+# EXP_NAME=alc_pretrain
+EXP_NAME=joint_pretrain_alc_train_val
 CHECKPOINT_NAME="model_last"
 
 sh scripts/test.sh \
@@ -28,5 +31,4 @@ sh scripts/test.sh \
     -g ${NUM_GPU} \
     -d ${DATASET_NAME} \
     -n ${EXP_NAME} \
-    -r ${RESUME} \
     -w ${CHECKPOINT_NAME}
